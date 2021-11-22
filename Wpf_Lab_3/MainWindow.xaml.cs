b46a1f05857a33623058882867334d99adf338c0
xbepxx@gmail.com <xbepxx@gmail.com>
2021-11-22 09:29:24 +0300

    1

--- Wpf_Lab_3/MainWindow.xaml.cs
+++ Wpf_Lab_3/MainWindow.xaml.cs
@@ -26,8 +26,23 @@ namespace Wpf_Lab_3
         public MainWindow()
         {
             InitializeComponent();
+            List<string> styles = new List<string>() {"Светлая тема", "Темная тема"};
+            styleBox.ItemsSource = styles;
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.SelectedIndex = 0;
         }
 
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = styleBox.SelectedIndex;
+            Uri uri = new Uri("WB.xaml", UriKind.Relative);
+            if (styleIndex==1)
+            {
+                uri=new Uri("BW.xaml", UriKind.Relative);
+            }
+            ResourceDictionary resource1 = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Add(resource1);
+        }
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             string fontName = ((sender as ComboBox).SelectedItem as String);
